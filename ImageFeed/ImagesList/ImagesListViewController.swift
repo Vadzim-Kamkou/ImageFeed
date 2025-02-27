5b230738fdc658a9871fe70d9fbaa1a0c8f8aa2c
Vadzim-Kamkou <vadzim.kamkou@gmail.com>
2025-02-27 22:24:28 +0300

    Custom ImagesListCell Added

---
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -0,0 +1,47 @@
+import UIKit
+
+
+class ImagesListViewController: UIViewController {
+
+    @IBOutlet private var tableView: UITableView!
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+//        tableView.delegate = self
+//        tableView.dataSource = self
+//        tableView.register(ImagesListCell.self, forCellReuseIdentifier: ImagesListCell.reuseIdentifier)
+        tableView.rowHeight = 200
+    }
+    
+}
+
+extension ImagesListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    }
+}
+
+extension ImagesListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return  1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath)
+        
+        guard let imagesListCell = cell as? ImagesListCell else {
+            print("Неуспешное привидение типа ячейки. Ошибка создания imageListCell.")
+            return UITableViewCell()
+        }
+        configCell(for: imagesListCell)
+        return imagesListCell
+
+    }
+    
+    
+}
+
+extension ImagesListViewController {
+    func configCell(for cell: ImagesListCell) { }
+}
