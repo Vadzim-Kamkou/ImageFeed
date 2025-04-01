02d6c8ff6720b92a39839a7c1d593b0f99d7862c
Vadzim-Kamkou <vadzim.kamkou@gmail.com>
2025-04-01 17:42:25 +0300

    Unsplash API OAuth2 authorization code Received

--- ImageFeed/Constants.swift
+++ ImageFeed/Constants.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-enum Constant {
+enum Constants {
     static let accessKey = "6GmCG_FavmwP9oNwLk0k_vD2YNvp0kh4a8mduYi_TDw"
     static let secretKey = "9O4dH0H-U3WZWljUP7ZnjQBd9YAlNLCY9_ku6xgioeQ"
     static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
